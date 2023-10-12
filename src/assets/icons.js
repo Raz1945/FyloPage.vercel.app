02c36f05ff6070590fc91cc1eb2db6aec52cdaf6
Raz1945 <g.sebas.sanchez@gmail.comm>
2023-10-12 10:29:00 -0300

    update icon src

---
+++ src/assets/icons.js
@@ -0,0 +1,15 @@
+export const icons = {
+  curvy: {
+    desktop: '/icons/bg-curvy-desktop.svg',
+    mobile: '/icons/bg-curvy-mobile.svg',
+  },
+  access: '/icons/icon-access-anywhere.svg',
+  any_file: '/icons/icon-any-file.svg',
+  arrow: '/icons/icon-arrow.svg',
+  collaboration: '/icons/icon-collaboration.svg',
+  email: '/icons/icon-email.svg',
+  location: '/icons/icon-location.svg',
+  phone: '/icons/icon-phone.svg',
+  security: '/icons/icon-security.svg',
+  logo: '/icons/logo.svg',
+};
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,9 @@
-import "./css/style.css"
 import { Logo } from "./components/logoFooter";
 import { Item } from "./components/itemFooter";
 import { LitaDeEnlaces } from "./components/listasDeEnlaceFooter";
 import { Redes } from "./components/redesFooter";
+import "./css/style.css"
+import { icons } from "../../assets/icons";
 
 export const Footer = () => (
     <>
@@ -11,7 +12,7 @@ export const Footer = () => (
             <div className="footer__bottom">
                 <div className="footer__box-01">
                     <Item
-                        src="/icons/icon-location.svg"
+                        src={icons.location}
                         alt="Icono de Ubicacion"
                         text="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                         Deleniti quos similique nam commodi voluptatum"
@@ -19,12 +20,12 @@ export const Footer = () => (
                 </div>
                 <div className="footer__box-02">
                     <Item
-                        src="/icons/icon-phone.svg"
+                        src={icons.phone}
                         alt="Icono de telefono"
                         text="+1-543-123-4567"
                     />
                     <Item
-                        src="/icons/icon-email.svg"
+                        src={icons.email}
                         alt="Icono de email"
                         text="example@fylo.com"
                     />
